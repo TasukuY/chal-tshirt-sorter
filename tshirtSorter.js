cd75570df93ade177dafd13a1e1a790176fa1e90
TasukuY <alohaty81@gmail.com>
2022-03-01 17:02:09 -0700

    coding challenge done

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -1,1 +1,33 @@
 // Write your solution below:
+/*
+Write a function that will take in a string containing only s, m, and l characters. Ex: slsmmsllsmsmlmsls
+This string essentially represents a bunch of t-shirts in an unsorted pile. 
+Your function should return this pile of t-shirts sorted by small, then medium, then large. 
+The above example would be returned sssssssmmmmmlllll.
+The given string will never include characters outside of the lowercase s, m, and l. The string will also never contain any spaces.
+
+>>> tshirtSorter('lms')
+// sml
+
+>>> tshirtSorter('smllms')
+// ssmmll
+*/
+
+const tshirtSorter = str => {
+    let sStr = '';
+    let mStr = '';
+    let lStr = '';
+
+    for(let i = 0; i < str.legnth; i++){
+        if(str[i] === 's'){
+            sStr += str[i];
+        }else if(str[i] === 'm'){
+            mStr += str[i];
+        }else{
+            lStr += str[i];
+        }
+    }
+    return sStr+mStr+lStr;
+}
+
+
